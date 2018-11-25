6bbe815ea22cf2790022a866cdbe0f8338c48e36
Panot Sodsri <panotsodsri@gmail.com>
2018-11-25 18:17:36 +0700

    vet fav system and fix bug event

--- DogetorProject/routes/vetSchema.js
+++ DogetorProject/routes/vetSchema.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var vet = new Schema({
     name: String,
+    surname:String,
     hos: String,
     phone: String,    
     pic: String
